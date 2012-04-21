ee89d8ab71857584aa979923200298c50e69fdab
Justin Ridgewell <jridgewe@stetson.edu>
2012-04-20 21:02:55 -0400

    Add VERY basic Customer class.

---
+++ Customer.cpp
@@ -0,0 +1,133 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Customer.cpp
+ *
+ *    Description:  Class implementation for Customer
+ *
+ *        Version:  1.0
+ *        Created:  04/20/2012 20:11:42
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Justin Ridgewell
+ *   Organization:  Team Dragonite
+ *
+ * =====================================================================================
+ */
+#include "Customer.h"
+
+Customer::Customer(){
+	this->setCustomerID(0);
+	this->setUsername("username");
+	this->setPassword("password");
+	this->setFullName("First Last");
+	this->setAddress("123 Street Dr.");
+	this->setCity("City");
+	this->setState("State");
+	this->setZip("12345");
+	this->setMoney(0);
+}
+
+Customer::Customer(const int customerID, const std::string username, const std::string password, const std::string fullName, const std::string address, const std::string city, const std::string state, const std::string zip, const int money) {
+	this->setCustomerID(customerID);
+	this->setUsername(username);
+	this->setPassword(password);
+	this->setFullName(fullName);
+	this->setAddress(address);
+	this->setCity(city);
+	this->setState(state);
+	this->setZip(zip);
+	this->setMoney(money);
+}
+
+Customer::Customer(const Customer& customer){
+	this->setCustomerID(customer.myCustomerID);
+	this->setUsername(customer.myUsername);
+	this->setPassword(customer.myPassword);
+	this->setFullName(customer.myFullName);
+	this->setAddress(customer.myAddress);
+	this->setCity(customer.myCity);
+	this->setState(customer.myState);
+	this->setZip(customer.myZip);
+	this->setMoney(customer.myMoney);
+}
+
+const int Customer::getCustomerID() const{
+	return myCustomerID;
+}
+
+const std::string Customer::getUsername() const{
+	return myUsername;
+}
+
+const std::string Customer::getPassword() const{
+	return myPassword;
+}
+
+const std::string Customer::getFullName() const{
+	return myFullName;
+}
+
+const std::string Customer::getAddress() const{
+	return myAddress;
+}
+
+const std::string Customer::getCity() const{
+	return myCity;
+}
+
+const std::string Customer::getState() const{
+	return myState;
+}
+
+const std::string Customer::getZip() const{
+	return myZip;
+}
+
+const int Customer::getMoney() const{
+	return myMoney;
+}
+
+void Customer::setCustomerID(const int customerID){
+	myCustomerID = customerID;
+}
+
+void Customer::setUsername(const std::string username){
+	myUsername = username;
+}
+
+void Customer::setPassword(const std::string password){
+	myPassword = password;
+}
+
+void Customer::setFullName(const std::string fullName){
+	myFullName = fullName;
+}
+
+void Customer::setAddress(const std::string address){
+	myAddress = address;
+}
+
+void Customer::setCity(const std::string city){
+	myCity = city;
+}
+
+void Customer::setState(const std::string state){
+	myState = state;
+}
+
+void Customer::setZip(const std::string zip){
+	myZip = zip;
+}
+
+void Customer::setMoney(const int money){
+	myMoney = money;
+}
+
+#ifdef TEST
+int main() {
+	Customer c = Customer();
+	std::cout << c.getFullName() << std::endl;
+}
+#endif
---
+++ Customer.h
@@ -0,0 +1,58 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+/*
+ * =====================================================================================
+ *
+ *       Filename:  Customer.h
+ *
+ *    Description:  Header file for Customer class
+ *
+ *        Version:  1.0
+ *        Created:  04/20/2012 20:11:42
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Justin Ridgewell
+ *   Organization:  Team Dragonite
+ *
+ * =====================================================================================
+ */
+#include <iostream>
+#include <string> 
+
+class Customer {
+	private:
+		int myCustomerID;
+		std::string myUsername;
+		std::string myPassword;
+		std::string myFullName;
+		std::string myAddress;
+		std::string myCity;
+		std::string myState;
+		std::string myZip;
+		int myMoney;
+	public:
+		Customer();
+	Customer(const int customerID, const std::string username, const std::string password, const std::string fullName, const std::string address, const std::string city, const std::string state, const std::string zip, const int money);
+		Customer(const Customer& customer);
+		virtual ~Customer() {}
+		const int getCustomerID() const;
+		const std::string getUsername() const;
+		const std::string getPassword() const;
+		const std::string getFullName() const;
+		const std::string getAddress() const;
+		const std::string getCity() const;
+		const std::string getState() const;
+		const std::string getZip() const;
+		const int getMoney() const;
+		void setCustomerID(const int customerID);
+		void setUsername(const std::string username);
+		void setPassword(const std::string password);
+		void setFullName(const std::string fullName);
+		void setAddress(const std::string address);
+		void setCity(const std::string city);
+		void setState(const std::string state);
+		void setZip(const std::string zip);
+		void setMoney(const int money);
+};
+#endif

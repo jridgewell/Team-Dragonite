cccaed7c07ce42829002d2a136e6f2083f0eb332
jjumalon <jjumalon@stetson.edu>
2012-05-03 14:47:05 -0400

    Readded Category parameter constructor using header file shortcut

--- Category.h
+++ Category.h
@@ -20,7 +20,8 @@ class Category
 
 	public:
 		Category();
-		Category(const int categoryID, const std::string& categoryName);
+		Category(const int categoryID, const std::string& categoryName)
+			: myCategoryID(categoryID), myCategoryName(categoryName) {}		
 		Category(const Category& category);
 		Category(const YAML::Node& node);
 		virtual ~Category() {}

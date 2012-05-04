cc47f2a9661ccf8873ef7064e642c3068def7d54
Scott Kieronski <skierons@stetson.edu>
2012-05-04 14:42:48 -0400

    Revised Inventory_unittest.cpp

--- unittest/Inventory_unittest.cpp
+++ unittest/Inventory_unittest.cpp
@@ -20,14 +20,14 @@
 #include <gtest/gtest.h>
 
 /*
-* Test the regular constructor for the merchant class
+* Test the regular constructor for the Inventory class
 */
-TEST(Inventory, Constructor)
+/*TEST(Inventory, Constructor)
 {
         const Inventory i("merchant", "pass");
         EXPECT_EQ(i.getUsername(), "merchant");
         EXPECT_EQ(i.getPassword(), "pass");
-}
+}*/
 
 /*
 * Test the default constructor 
@@ -35,8 +35,11 @@ TEST(Inventory, Constructor)
 TEST(Inventory, DefaultConstructor)
 {
         const Inventory i;
-        EXPECT_EQ(i.getUsername(), "username");
-        EXPECT_EQ(i.getPassword(), "password");
+        EXPECT_EQ(i.getSKU(), 0);
+        EXPECT_EQ(i.getItemDesc(), "Item Description");
+        EXPECT_EQ(i.getCategoryID(), 0);
+        EXPECT_EQ(i.getPrice(), 0.0);
+        EXPECT_EQ(i.getQuantity(), 0);
 }
 
 /*
@@ -46,6 +49,9 @@ TEST(Inventory, CopyConstructor)
 {
         const Inventory i;
         const Inventory i2 = i;
-        EXPECT_EQ(m.getUsername(), m2.getUsername());
-        EXPECT_EQ(m.getPassword(), m2.getPassword());
+        EXPECT_EQ(i.getSKU(), i2.getSKU());
+        EXPECT_EQ(i.getItemDesc(), i2.getItemDesc());
+        EXPECT_EQ(i.getCategoryID(), i2.getCategoryID());
+        EXPECT_EQ(i.getPrice(), i2.getPrice());
+        EXPECT_EQ(i.getQuantity(), i2.getQuantity());
 }

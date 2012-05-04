9cb81e19e38bebf69ff499a5c147f7e40724cfe6
Justin Ridgewell <jridgewe@stetson.edu>
2012-05-04 13:10:44 -0400

    Fix Jure's Last Commit
    Pasting doesn't work in VIM unless you're in insert mode!

--- Inventory.cpp
+++ Inventory.cpp
@@ -1,178 +1,114 @@
-nvenctory
-
+/**
+* Invenctory
 * @date May 2, 2012
-
 * @authoer Jure Jumalon
-
 */
 
 #include "Inventory.h"
 
 Inventory::Inventory()
-
 /**
-
  * Purpose: Inventory constructor
-
  * Post: Inventory object created
-
  * 			SKU = 0
-
  * 			Item Description = "Item Description"
-
  * 			Category ID = 0
-
  * 			Price = 0.0
-
  * 			Quantity = 0
-
  */
-
 {
-
 	this -> setSKU(0);
-
 	this -> setItemDesc("Item Description");
-
 	this -> setCategoryID(0);
-
 	this -> setPrice(0.0);
-
 	this -> setQuantity(0);
-
 }
 
 Inventory::Inventory(const Inventory& inventory)
-
 /**
-
  * Purpose: Copy Inventory constructor
-
  * Post: Inventory object created as a copy of inventory
-
  */
-
 {
-
 	this -> setSKU(inventory.mySKU);
-
 	this -> setItemDesc(inventory.myItemDesc);
-
 	this -> setCategoryID(inventory.myCategoryID);
-
 	this -> setPrice(inventory.myPrice);
-
 	this -> setQuantity(inventory.myQuantity);
-
 }
 
 Inventory::Inventory(const YAML::Node& node)
-
 {
-
 	this -> parseYaml(node);
-
 }
 
 void Inventory::parseYaml(const YAML::Node& node)
-
 {
-
 	node["mySKU"] >> this->mySKU;
-
 	node["myItemDesc"] >> this->myItemDesc;
-
 	node["myCategoryID"] >> this->myCategoryID;
-
 	node["myPrice"] >> this->myPrice;
-
 	node["myQuantity"] >> this->myQuantity;
-
 }
 
+
 /*
-
  * Accessors and Mutators
-
  */
 
 int Inventory::getSKU() const
-
 {
-
    return mySKU;
-
 }
 
 const std::string& Inventory::getItemDesc() const
-
 {
-
    return myItemDesc; 
-
 }
 
 int Inventory::getCategoryID() const
-
 {
-
    return myCategoryID;
-
 }
 
 float Inventory::getPrice() const
-
 {
-
    return myPrice;
-
 }
 
 int Inventory::getQuantity() const
-
 {
-
    return myQuantity;
-
 }
 
 void Inventory::setSKU(const int sku)
-
 {
-
 	mySKU = sku;
-
 }
 
 void Inventory::setItemDesc(const std::string& itemDesc)
-
 {
-
 	myItemDesc = itemDesc;
-
 }
 
 void Inventory::setCategoryID(int categoryID)
-
 {
-
 	myCategoryID = categoryID;
-
 }
 
 void Inventory::setPrice(const float price)
-
 {
-
    myPrice = price;
-
 }
 
 void Inventory::setQuantity(const int quantity)
-
 {
-
    myQuantity = quantity;
-
 }
+
+
+
+
+
+
 
--- Inventory.h
+++ Inventory.h
@@ -1,82 +1,48 @@
-fndef INVENTORY_H
-
+#ifndef INVENTORY_H
 #define INVENTORY_H
 
 /**
-
 * Inventory
-
 * @date May 2, 2012
-
 * @author Jure Jumalon
-
 *
-
 * TODO: Javadocs
-
 *
-
 */
 
 #include <iostream>
-
 #include <string>
-
 #include <yaml-cpp/yaml.h>
 
 class Inventory
-
 {
-
    private:
-
       int mySKU;
-
       std::string myItemDesc;
-
       int myCategoryID;
-
       float myPrice;
-
       int myQuantity;
 
    public:
-
       Inventory();
-
       Inventory(const int sku, const std::string& itemDesc, const int categoryID, const float price, const int quantity)
-
       	  : mySKU(sku), myItemDesc(itemDesc), myCategoryID(categoryID), myPrice(price), myQuantity(quantity) {}
-
       Inventory(const Inventory& inventory);
-
       Inventory(const YAML::Node& node);
-
       virtual ~Inventory() {}
-
       void parseYaml(const YAML::Node& node);
 
       int getSKU() const;
-
       const std::string& getItemDesc() const;
-
       int getCategoryID() const;
-
       float getPrice() const;
-
       int getQuantity() const;
 
       void setSKU(const int sku);
-
       void setItemDesc(const std::string& itemDesc);
-
       void setCategoryID(int categoryID);
-
       void setPrice(const float price);
-
       void setQuantity(const int quantity); 
-
 };
 
 #endif
-

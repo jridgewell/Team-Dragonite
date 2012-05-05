9368c10f67e34f244f25d1f5022a82892a6a4eeb
jjumalon <jjumalon@stetson.edu>
2012-05-05 13:03:02 -0400

    Added Controller.cpp

---
+++ Controller.cpp
@@ -0,0 +1,38 @@
+Controller::Controller()
+{
+}
+
+virtual Controller::~Controller()
+{
+}
+
+Controller::displayCustomerLogin() const
+{
+}
+
+const Customer::Customer& customer Controller::customerLogin(const std::string& username, const std::string& password)
+{
+}
+
+const Customer::Customer& customer Controller::createCustomer(const std::string& username, const std::string& password)
+{
+}
+
+Controller::displayMerchantLogin() const
+{
+}
+
+const Customer::Merchant& merchant Controller::merchantLogin(const std::string& username, const std::string& password) const
+{
+}
+
+Controller::setUserID(int id)
+{
+	myUserID = id;
+}
+
+const int getUserID() const
+{
+	return myUserID;
+}
+
---
+++ Controller.h
@@ -0,0 +1,31 @@
+#ifndef CONTROLLER_H
+#define CONTROLLER_H
+
+#include <iostream>
+
+class Controller
+{
+private:
+	int myUserID;
+	map<string, Category*> myCategories;
+	map<string, Merchant*> myMerchants;
+	vector<Customer*> myCustomers;
+	vector<inventory*> myInventories;
+	vector<Order*> myOrder;
+
+public:
+	Controller();
+	virtual ~Controller();
+
+	displayCustomerLogin() const;
+	const Customer::Customer& customer customerLogin(const std::string& username, const std::string& password);
+	const Customer::Customer& customer createCustomer(const std::string& username, const std::string& password);
+
+	displayMerchantLogin() const;
+	const Customer::Merchant& merchant merchantLogin(const std::string& username, const std::string& password) const;
+
+	setUserID(int id);
+	const int getUserID() const;
+};
+
+#endif

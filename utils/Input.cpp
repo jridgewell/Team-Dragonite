7c9bef8c4157979b58dc9a3a2e252f12e7c35c89
Justin Ridgewell <jridgewe@stetson.edu>
2012-05-07 00:33:54 -0400

    Add Input::getPositiveInteger

--- utils/Input.cpp
+++ utils/Input.cpp
@@ -21,6 +21,16 @@ bool Input::getInteger(int& i, std::istream& in, const char delim) {
 	return false;
 }
 
+bool Input::getPositiveInteger(int& i, std::istream& in, const char delim) {
+	std::string str;
+	Input::getLine(str);
+	if (Input::isNumericPositive(str)) {
+		i = atoi(str.c_str());
+		return true;
+	}
+	return false;
+}
+
 bool Input::isNumeric(const std::string& str) {
 	unsigned i = 0;
 	if (str[0] == '-') {
--- utils/Input.h
+++ utils/Input.h
@@ -10,6 +10,7 @@
 struct Input {
 	static std::istream& getLine(std::string& str, std::istream& in = std::cin, const char delim = '\n');
 	static bool getInteger(int& i, std::istream& in = std::cin, const char delim = '\n');
+	static bool getPositiveInteger(int& i, std::istream& in = std::cin, const char delim = '\n');
 	static bool isNumeric(const std::string& str);
 	static bool isNumericPositive(const std::string& str);
 	static void wait(const std::string str = std::string("Press enter to continue"), std::ostream& out = std::cout, std::istream& in = std::cin, const char delim = '\n');

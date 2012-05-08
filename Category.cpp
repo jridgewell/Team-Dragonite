fe0c2de723609d12308c821f94418a0665f2818d
Andrew W <awagenhe@stetson.edu>
2012-05-07 20:19:34 -0400

    Added comments to Category.cpp

--- Category.cpp
+++ Category.cpp
@@ -29,17 +29,32 @@ Category::Category(const Category& category)
 	this -> setCategoryName(category.myCategoryName);
 }
 
+/**
+* Pushes data from a category onto a node for storage
+* Pre: A node from the YAML is passed in
+* Post: The data from the category is stored on the input node
+*/
 Category::Category(const YAML::Node& node)
 {
 	this -> parseYaml(node);
 }
 
+/**
+* Creates a new category from data stored on the YAML
+* Pre: A node from the YAML is passed in
+* Post: A category is created from the input node
+*/
 void Category::parseYaml(const YAML::Node& node)
 {
 	node["myCategoryID"] >> this -> myCategoryID;
 	node["myCategoryName"] >> this -> myCategoryName;
 }
 
+/**
+* Stores data on the YAML as categories are created
+* Pre: An emitter is passed in to track changes
+* Post: The newly made categories are stored on the YAML
+*/
 YAML::Emitter& Category::emitYaml(YAML::Emitter& out) const
 {
 	out << YAML::BeginMap;
